3d68072ae86252479a3a3bd91f768b13da780e9d
chintaman2004 <ahmed.hassanc@gmail.com>
2025-05-31 17:16:30 +0500

    Errors Removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,16 +18,16 @@ class LudoApp extends StatelessWidget {
 class LudoBoard extends StatelessWidget {
   final int gridSize = 15;
 
-  const LudoBoard({super.key}); // 15x15 Ludo board
+  const LudoBoard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Simple Ludo Board")),
+      appBar: AppBar(title: Text("Ludo Board")),
       backgroundColor: Colors.blueAccent,
       body: Center(
         child: AspectRatio(
-          aspectRatio: 1, // make sure it's a square
+          aspectRatio: 1,
           child: GridView.builder(
             itemCount: gridSize * gridSize,
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -37,19 +39,14 @@ class LudoBoard extends StatelessWidget {
 
               Color color = Colors.white;
 
-              // Red Home (Top-Left)
-              if (row < 6 && col < 6)
+              if (row < 6 && col < 6) {
                 color = Colors.red[300]!;
-              // Green Home (Top-Right)
-              else if (row < 6 && col > 8)
+              } else if (row < 6 && col > 8)
                 color = Colors.green[300]!;
-              // Yellow Home (Bottom-Left)
               else if (row > 8 && col < 6)
                 color = Colors.yellow[300]!;
-              // Blue Home (Bottom-Right)
               else if (row > 8 && col > 8)
                 color = Colors.blue[300]!;
-              // Middle Path
               else if (col == 7 || row == 7)
                 color = Colors.grey[300]!;
 
